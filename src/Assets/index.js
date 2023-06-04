08171cc0d39aeb54a08e7ef71bfa6a7ad298081a
jorgeProgramaEn5 <116350790+jorgeProgramaEn5@users.noreply.github.com>
2023-06-04 11:57:07 -0400

    start

---
+++ src/Assets/index.js
@@ -0,0 +1,17 @@
+import facebook from "./facebook.svg";
+import linkedin from "./linkedin.svg";
+import instagram from "./instagram.svg";
+import twitter from "./twitter.svg";
+import logo from "./logo.png";
+import header720x720 from "./header720x720.png";
+
+
+
+export {
+  facebook,
+  linkedin,
+  instagram,
+  twitter,
+  logo,
+  header720x720,
+};
---
+++ src/Constant/index.js
@@ -0,0 +1,24 @@
+import {instagram, twitter, facebook, linkedin} from '../Assets';
+
+  export const socialMedia = [
+    {
+      id: "social-media-1",
+      icon: instagram,
+      link: "https://www.instagram.com/",
+    },
+    {
+      id: "social-media-2",
+      icon: facebook,
+      link: "https://www.facebook.com/",
+    },
+    {
+      id: "social-media-3",
+      icon: twitter,
+      link: "https://www.twitter.com/",
+    },
+    {
+      id: "social-media-4",
+      icon: linkedin,
+      link: "https://www.linkedin.com/",
+    },
+  ];
