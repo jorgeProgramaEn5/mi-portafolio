c7c9a9accadb7e4f558b6fe2b14202f8d2d40880
jorgeProgramaEn5 <jorgitoestrada2924@gmail.com>
2023-06-06 16:06:13 -0500

    add img

--- src/Assets/index.js
+++ src/Assets/index.js
@@ -9,7 +9,22 @@ import met_2 from "./met_2.png";
 import met_3 from "./met_3.png";
 import met_4 from "./met_4.png";
 
-
+import arrow1 from "./arrow1.svg";
+import arrow2 from "./arrow2.svg";
+import color_sharp from "./color_sharp.png";
+import color_sharp2 from "./color_sharp2.png";
+import contact_img from "./contact_img.svg";
+import footer_bg from "./footer_bg.png";
+import header_img from "./header_img.svg";
+import meter1 from "./meter1.svg";
+import meter2 from "./meter2.svg";
+import meter3 from "./meter3.svg";
+import nav_icon1 from "./nav_icon1.svg";
+import nav_icon2 from "./nav_icon2.svg";
+import nav_icon3 from "./nav_icon3.svg";
+import project_img1 from "./project_img1.png";
+import project_img2 from "./project_img2.png";
+import project_img3 from "./project_img3.png";
 
 
 export {
@@ -23,4 +38,20 @@ export {
   met_2,
   met_3,
   met_4,
+  arrow1,
+  arrow2,
+  color_sharp,
+  color_sharp2,
+  contact_img,
+  footer_bg,
+  header_img,
+  meter1,
+  meter2,
+  meter3,
+  nav_icon1,
+  nav_icon2,
+  nav_icon3,
+  project_img1,
+  project_img2,
+  project_img3
 };
