ab149490f605fb09a4dd1c84b41288666c10f3b6
jorgeProgramaEn5 <jorgitoestrada2924@gmail.com>
2023-06-17 09:00:44 -0500

    add contactemailjs

--- src/Components/ContactEmailjs.jsx
+++ src/Components/ContactEmailjs.jsx
@@ -10,6 +10,8 @@ function Contact() {
 
     const form = useRef();
     const [buttonText, setButtonText] = useState("send");
+    const [buttonTextDesable, setButtonTextDesable] = useState("text-enable");
+    const [buttonTextEnable, setButtonTextEnable] = useState("text-desable");
 
 
     const sendEmail = (e) => {
@@ -19,6 +21,8 @@ function Contact() {
         .then((result) => {
             console.log(result.text);
             setButtonText("Message sent successfully")
+            setButtonTextDesable("text-desable")
+            setButtonTextEnable("text-enable")
         }, (error) => {
             console.log(error.text);
             setButtonText("Ups! Something went wrong")
@@ -32,33 +36,34 @@ function Contact() {
                   <Col md={6} className='imgContact'>
                       <img src={contact_img} alt="contact" />
                   </Col>
-                  <Col md={6} className='contact-form'>
-                      <h2>Get In Touch</h2>
-                      <form ref={form} onSubmit={sendEmail}>
-                          <Row>
-                              <Col sm={6} xs={6} className='px-1'>
-                                  <input type="text" name="from_name"  placeholder='First Name'/>
-                              </Col>
-  
-                              <Col sm={6} xs={6} className='px-1'>
-                                  <input type="text" name="from_lastname" placeholder='Last Name'/>
-                              </Col>
-  
-                              <Col sm={6} xs={6} className='px-1'>
-                                  <input type="email" name="from_email" placeholder='Email Address'/>
-                              </Col>
-  
-                              <Col sm={6} xs={6} className='px-1'>
-                                  <input type="tel" name="phone" placeholder='Phone Number'/>
-                              </Col>
-  
-                              <Col className='col-text-area'>
-                                  <textarea style={{ resize: "none" }} name="message" row="6" placeholder='Message'></textarea>
-                                  <input type="submit" value={buttonText} />
-                              </Col>
-                          </Row>
-                      </form>
-                  </Col>
+                    <Col md={6} className='contact-form'>
+                        <h2>Get In Touch</h2>
+                        <form ref={form} onSubmit={sendEmail}>
+                            <Row>
+                                <Col sm={6} xs={6} className='px-1'>
+                                    <input type="text" name="from_name"  placeholder='First Name' required/>
+                                </Col>
+    
+                                <Col sm={6} xs={6} className='px-1'>
+                                    <input type="text" name="from_lastname" placeholder='Last Name'/>
+                                </Col>
+    
+                                <Col sm={6} xs={6} className='px-1'>
+                                    <input type="email" name="from_email" placeholder='Email Address' required/>
+                                </Col>
+    
+                                <Col sm={6} xs={6} className='px-1'>
+                                    <input type="tel" name="phone" placeholder='Phone Number' required/>
+                                </Col>
+    
+                                <Col className='col-text-area'>
+                                    <textarea style={{ resize: "none" }} name="message" row="6" placeholder='Message' required></textarea>
+                                    <input type="submit" value="send" className={buttonTextDesable}/>
+                                    <input type="submit" value={buttonText} className={buttonTextEnable} disabled/>
+                                </Col>
+                            </Row>
+                        </form>
+                    </Col>
               </Row>
           </Container>
       </section>
